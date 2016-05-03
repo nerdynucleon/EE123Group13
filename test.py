ca5b4027f129736eaecf3e65aad074c98fd78ff2
R. Menon <ramspeedy@gmail.com>
2016-05-03 15:48:10 -0700

    le changes for le win

--- test.py
+++
@@ -1,9 +0,0 @@
-from main import main
-import time
-
-print time.strftime("%Y-%m-%d %H:%M")
-main(display = False, img_files = ['imgs/Marconi.tiff'])
-main(display = False, img_files = ['imgs/Isee.tiff'])
-main(display = False, img_files = ['imgs/calBlue.tiff'])
-main(display = False, img_files = ['imgs/createrLake.tiff'])
-main(display = False, img_files = ['imgs/pauly.tiff'])
